af88d6744a7841395a33664fe54372fb5e11862d
Bumsik Kim <k.bumsik@gmail.com>
2019-12-19 17:50:34 +0900

    Crawler: Add basic database

--- blogsearch-crawler/index.ts
+++ blogsearch-crawler/index.ts
@@ -1,6 +1,10 @@
 import * as puppeteer from 'puppeteer';
 import * as path from 'path';
 import * as process from 'process';
+// This is an "promisified" version of sqlite3
+import * as sqlite from 'sqlite';
+// This is only need for constants declarations
+import {OPEN_READONLY, OPEN_CREATE, OPEN_READWRITE} from 'sqlite3';
 
 type inputFields = object;
 
@@ -21,8 +25,13 @@ export default async function(config: IConfig) {
   const browser = await puppeteer.launch({
     ignoreHTTPSErrors: true,
   });
+  const db = await sqlite.open(config.output,
+    { mode: (OPEN_CREATE | OPEN_READWRITE), verbose: true });
+  const keys = Object.keys(config.fields);
+  let stmt = await db.run(`CREATE VIRTUAL TABLE blogsearch USING fts5(${keys.join(',')}, detail=full);`);
 
-  const crawl = async (input: IInput) => {
+  // The crawler task
+  const crawlerTask = async (input: IInput) => {
     const context = await browser.createIncognitoBrowserContext();
     for (let include; include = input.includes.pop();) {
       const page = await context.newPage();
@@ -30,25 +39,33 @@ export default async function(config: IConfig) {
       await page.goto(include);
       // TODO: what if include is a directory?
 
+      const parsedFields: any = {};
       for (const [key, selector] of Object.entries(input.selectors)) {
         try {
           const parsed = await page.$eval(selector, el => el.textContent);
-          console.log(parsed);
+          parsedFields[key] = parsed?.toString();
         } catch (err) {
           throw new Error (`Parsing failed: ${key} field in ${include}. :${err}`);
         }
       }
+
+      // A single quote should be encoded to two single quotes: https://www.sqlite.org/lang_expr.html
+      const parameters = keys.map((key) =>
+          `'${ parsedFields[key].replace(/'/g, "''") }'`
+        ).join(',');
+      stmt = await db.run(`INSERT INTO blogsearch VALUES (${parameters});`);
       page.close();
     }
     await context.close();
     return;
   };
 
+  // Start crawler and finish
   for (const input of config.inputs) {
-    // TODO: configurable number of crawlers
-    await Promise.all((new Array(4)).fill(crawl(input)));
+    // TODO: configurable number of tasks
+    await Promise.all((new Array(4)).fill(crawlerTask(input)));
   }
-
+  db.close();
   browser.close();
 }
 
