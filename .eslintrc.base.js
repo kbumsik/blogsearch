958beb46a1d9a0cf9d0402848df4efbfea4e3029
Bumsik Kim <k.bumsik@gmail.com>
2019-12-23 21:46:32 +0900

    Prototype example page

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -1,9 +1,40 @@
 module.exports = {
-  extends: [
-    'algolia'
-  ],
+  env: {
+    browser: true,
+    node: true
+  },
   rules: {
     "eslint-comments/disable-enable-pair": "off",
-    "eslint-comments/no-unlimited-disable": "off",
-  }
+  },
+  overrides: [
+    {
+      // JavaScript
+      files: ['*.js'],
+      extends: [
+        'algolia',
+      ],
+    },
+    {
+      // TypeScript
+      files: ['*.ts'],
+      extends: [
+        'algolia/typescript'
+      ],
+      rules: {
+        // These does not resolve .ts files. Is it bug?
+        "import/extensions": "off",
+        "import/no-unresolved": "off",
+        // This does not allow "T" as generic type naming, forcing to use unnecessary lengthy "TType" or something.
+        "@typescript-eslint/generic-type-naming": "off"
+      }
+    },
+    {
+      // Our blogsearch.config.js is for demontrations to users.
+      // Therefore we choose making easier to understand for users over correcteness by the formatters.
+      files: ['*blogsearch.config.js'],
+      rules: {
+        "eslint-comments/no-unlimited-disable": "off",
+      }
+    }
+  ],
 };
--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,10 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import initSearch from 'blogsearch';
+import blogsearch from 'blogsearch';
 
-initSearch({
+window['blogsearch'] = blogsearch;
+
+window['blogsearch']({
   dbPath: 'reactjs.org.blogsearch.db.bin',
   wasmPath: 'sql-wasm.wasm',
-}).then(sql => {
-  // eslint-disable-next-line dot-notation
-  window['blogsearch'] = sql;
-});
+}).then( _ => console.log('Ready'));
+
+// window['blogsearch'].load();
