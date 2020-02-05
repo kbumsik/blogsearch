efd8e20674b676270239aa7d38330050ff75e5db
Bumsik Kim <k.bumsik@gmail.com>
2020-02-05 14:39:40 +0900

    Build test to the new lib-test

--- blogsearch/jest.config.js
+++ blogsearch/jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
-  testMatch: ['**/lib/**/__tests__/**/*.js']
+  testMatch: ['**/lib-test/**/__tests__/**/*.js']
 };
 /* eslint-enable import/no-commonjs */
