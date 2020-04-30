271eabf1dc09c1d68d5a38aeb7b6fe58dbbe80cc
Bumsik Kim <k.bumsik@gmail.com>
2020-05-01 00:11:59 +0900

    jekyll-circleci: build js before jekyll

--- examples/jekyll-examples/circleci-docs/build-database.sh
+++ examples/jekyll-examples/circleci-docs/build-database.sh
@@ -8,7 +8,9 @@ VERSION="05f4b70c8a534119f2b708691659d79e95e69240"
 DATE="$(date +'%Y-%m-%d-%H-%M-%S')"
 
 cd "$REPO_DIR"
-docker-compose up
+npm install
+npm run webpack-prod
+JEKYLL_UID=$(id -u) JEKYLL_GID=$(id -g) docker-compose up --build
 cp *.db.wasm ../
 
 cd "$CUR_DIR"
