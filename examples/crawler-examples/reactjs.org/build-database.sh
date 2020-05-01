bef7ededebb1baeebb324ca522437ecbee2f8312
Bumsik Kim <k.bumsik@gmail.com>
2020-05-01 17:36:43 +0900

    crawler-reactjs: update scripts

---
+++ examples/crawler-examples/reactjs.org/build-database.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+REPO="reactjs.org"
+CUR_DIR=$(pwd)
+REPO_DIR="$CUR_DIR/$REPO"
+VERSION="dea4f329ea3a7bba116e07adf67eb5c8b6c528cd"
+DATE="$(date +'%Y-%m-%d-%H-%M-%S')"
+
+cd "$REPO_DIR"
+cp ../yarn.lock ./
+yarn --ignore-engines install
+yarn --ignore-engines build
+
+cd "$CUR_DIR"
+yarn blogsearch-crawler
