7c5377036a7f33368896e9fe697411cd5e639967
Bumsik Kim <k.bumsik@gmail.com>
2020-05-01 17:20:46 +0900

    examples: delete existing patches when updating

--- examples/gatsby-examples/reactjs.org/update-patches.sh
+++ examples/gatsby-examples/reactjs.org/update-patches.sh
@@ -7,7 +7,7 @@ REPO_DIR="$CUR_DIR/$REPO"
 VERSION="dea4f329ea3a7bba116e07adf67eb5c8b6c528cd"
 DATE="$(date +'%Y-%m-%d-%H-%M-%S')"
 
-rm -f "$CUR_DIR/*.patch"
+rm -f $CUR_DIR/*.patch
 cd "$REPO_DIR"
 git format-patch "$VERSION" -o "$CUR_DIR"
 
--- examples/jekyll-examples/circleci-docs/update-patches.sh
+++ examples/jekyll-examples/circleci-docs/update-patches.sh
@@ -7,7 +7,7 @@ REPO_DIR="$CUR_DIR/$REPO"
 VERSION="05f4b70c8a534119f2b708691659d79e95e69240"
 DATE="$(date +'%Y-%m-%d-%H-%M-%S')"
 
-rm -f "$CUR_DIR/*.patch"
+rm -f $CUR_DIR/*.patch
 cd "$REPO_DIR"
 git format-patch "$VERSION" -o "$CUR_DIR"
 
