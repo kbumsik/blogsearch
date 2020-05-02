6d30407a91dd754a1e38720f2b1f706987a2e652
Bumsik Kim <k.bumsik@gmail.com>
2020-05-02 13:50:34 +0900

    BlogSearch: redesign to use a factory instead of constructor

--- blogsearch/src/lib/BlogSearch.ts
+++ blogsearch/src/lib/BlogSearch.ts
@@ -2,6 +2,7 @@ import Hogan from 'hogan.js';
 import autocomplete, {
   Suggestion,
   AutocompleteOptions,
+  AutocompleteElement,
 } from './autocomplete.js';
 import SQLite, * as SQL from './sqlite';
 import templates from './templates';
@@ -34,10 +35,13 @@ blogsearch({
 })`;
 
 class BlogSearch {
-  private readonly sqlite: SQLite;
-  private readonly startAutoComplete: () => void;
 
-  public constructor ({
+  private constructor (
+    private readonly sqlite: SQLite,
+    private readonly autoComplete: AutocompleteElement,
+  ) {}
+
+  public static async create ({
     workerFactory,
     wasmPath = getCurrentDir('blogsearch.wasm'),
     dbPath = '',
@@ -51,170 +55,158 @@ class BlogSearch {
       ariaLabel: '',
     },
   }: Config & SQL.Config) {
-    checkArguments({ workerFactory, wasmPath, dbPath, inputSelector, debug, autocompleteOptions });
+    BlogSearch.checkArguments(arguments[0]);
 
-    this.sqlite = new SQLite({
+    const sqlite = await new SQLite({
       wasmPath,
       dbPath,
       worker: getWorkerFactory(workerFactory)(),
-    });
+    }).load();
+    const autoComplete = getAutoComplete();
+    return new BlogSearch(sqlite, autoComplete);
 
-    this.startAutoComplete = (() => {
-      const selector = BlogSearch.getInputFromSelector(inputSelector);
+    function getAutoComplete () {
+      const selector = getInputFromSelector(inputSelector);
       const template = Hogan.compile(templates.suggestion);
       const emptyTemplate = Hogan.compile(templates.empty);
-      const options = getAutocompleteOptions(autocompleteOptions, selector, debug);
-      return () => {
-        const inputHandler = autocomplete(selector, options,
-          [
-            {
-              source: this.getAutocompleteSource(),
-              templates: {
-                suggestion: (suggestion) => template.render(suggestion),
-                empty: (suggestion) => emptyTemplate.render(suggestion),
-              },
+      const autoComplete = autocomplete(
+        selector,
+        options(autocompleteOptions, selector, debug),
+        [
+          // Autocomplete sources
+          {
+            source: searchSource(),
+            templates: {
+              suggestion: (suggestion) => template.render(suggestion),
+              empty: (suggestion) => emptyTemplate.render(suggestion),
             },
-          ]
-        );
-        // Reference: https://github.com/algolia/autocomplete.js#events
-        inputHandler.on(
-          'autocomplete:selected',
-          this.handleSelected.bind(null, inputHandler.autocomplete)
-        );
-        inputHandler.on(
-          'autocomplete:shown',
-          this.handleShown.bind(null, selector)
-        );
+          },
+        ]
+      );
+      // Reference: https://github.com/algolia/autocomplete.js#events
+      autoComplete.on(
+        'autocomplete:selected',
+        handleSelected.bind(null, autoComplete.autocomplete)
+      );
+      autoComplete.on(
+        'autocomplete:shown',
+        handleShown.bind(null, selector)
+      );
+      return autoComplete;
+      
+      function options (
+        options: AutocompleteOptions,
+        input: JQuery<HTMLElement>,
+        debugFlag: boolean
+      ): AutocompleteOptions {
+        const inputAriaLabel = typeof input?.attr === 'function' ? input.attr('aria-label') : undefined;
+        return {
+          ...options,
+          debug: debugFlag ?? options.debug ?? false,
+          cssClasses: {
+            root: 'blogsearch-autocomplete',
+            prefix: 'bs',
+            ...options.cssClasses,
+          },
+          ariaLabel: options.ariaLabel ?? inputAriaLabel ?? 'search input',
+        };
       }
-    })();
-    return;
+      
+      /**
+       * Returns the `source` method to be passed to autocomplete.js. It will query
+       * the Algolia index and call the callbacks with the formatted hits.
+       * @function getAutocompleteSource
+       * @returns {function} Method to be passed as the `source` option of
+       * autocomplete
+       */
+      function searchSource () {
+        return async (
+          query: string,
+          showSearchResult: (suggestion: Suggestion[]) => void
+        ) => {
+          const suggestions = <Suggestion[]><unknown[]>await sqlite.search(query, 5);
+          showSearchResult(suggestions.map(suggestion => ({
+            ...suggestion,
+            tags: (<string>suggestion.tags ?? '')
+              .split(',')
+              .map(str => ({ value: str.trim() })),
+            categories: (<string>suggestion.categories ?? '')
+              .split(',')
+              .map(str => ({ value: str.trim() })),
+          })));
+          return;
+        };
+      }
 
-    function checkArguments (args: Config & SQL.Config) {
-      if (
-        typeof args.dbPath !== 'string' || !args.dbPath ||
-        typeof args.inputSelector !== 'string' || !args.inputSelector ||
-        (typeof args.workerFactory !== 'undefined' && typeof args.workerFactory !== 'function')
+      function handleSelected (
+        input: any,
+        _event: any,
+        suggestion: Suggestion,
+        _datasetNumber: any,
+        context: any = {},
       ) {
-        throw new Error(usage);
+        // Do nothing if click on the suggestion, as it's already a <a href>, the
+        // browser will take care of it. This allow Ctrl-Clicking on results and not
+        // having the main window being redirected as well
+        if (context.selectionMethod === 'click') {
+          return;
+        }
+
+        input.setVal('');
+        window.location.assign(suggestion.url);
       }
-      BlogSearch.getInputFromSelector(args.inputSelector);
-    }
 
-    function getWorkerFactory (factory?: () => Worker) {
-      if (typeof factory !== 'undefined') {
-        return factory;
+      function handleShown (input: JQuery<HTMLElement>) {
+        // @ts-ignore
+        const middleOfInput = input.offset().left + input.width() / 2;
+        // @ts-ignore
+        let middleOfWindow = $(document).width() / 2;
+
+        if (isNaN(middleOfWindow)) {
+          middleOfWindow = 900;
+        }
+
+        const alignClass =
+          middleOfInput - middleOfWindow >= 0
+            ? 'blogsearch-autocomplete-right'
+            : 'blogsearch-autocomplete-left';
+        const otherAlignClass =
+          middleOfInput - middleOfWindow < 0
+            ? 'blogsearch-autocomplete-right'
+            : 'blogsearch-autocomplete-left';
+        const autocompleteWrapper = $('.blogsearch-autocomplete');
+        if (!autocompleteWrapper.hasClass(alignClass)) {
+          autocompleteWrapper.addClass(alignClass);
+        }
+
+        if (autocompleteWrapper.hasClass(otherAlignClass)) {
+          autocompleteWrapper.removeClass(otherAlignClass);
+        }
       }
-      // Get current directory for worker
-      const workerDir = typeof window?.blogsearch?.worker === 'function'
-        ? URL.createObjectURL(new Blob([`(${window.blogsearch.worker})()`]))
-        : getCurrentDir('worker.umd.js');
-      return () => new Worker(workerDir);
-    }
-
-    function getAutocompleteOptions (
-      options: AutocompleteOptions,
-      input: JQuery<HTMLElement>,
-      debugFlag: boolean
-    ): AutocompleteOptions {
-      const inputAriaLabel = typeof input?.attr === 'function' ? input.attr('aria-label') : undefined;
-      return {
-        ...options,
-        debug: debugFlag ?? options.debug ?? false,
-        cssClasses: {
-          root: 'blogsearch-autocomplete',
-          prefix: 'bs',
-          ...options.cssClasses,
-        },
-        ariaLabel: options.ariaLabel ?? inputAriaLabel ?? 'search input',
-      };
     }
   }
 
-  public async load (): Promise<BlogSearch> {
-    await this.sqlite.load();
-    this.startAutoComplete();
-    return this;
-  }
-
-  /**
-   * Returns the matching input from a CSS selector, null if none matches
-   * @function getInputFromSelector
-   * @param  {string} selector CSS selector that matches the search
-   * input of the page
-   * @returns {void}
-   */
-  private static getInputFromSelector (selector: string) {
-    const input = $(selector).filter('input');
-    if (!input?.length) {
-      throw new Error(`Error: No input element in the page matches ${selector}`);
+  private static checkArguments (args: Config & SQL.Config) {
+    if (
+      typeof args.dbPath !== 'string' || !args.dbPath ||
+      typeof args.inputSelector !== 'string' || !args.inputSelector ||
+      (typeof args.workerFactory !== 'undefined' && typeof args.workerFactory !== 'function')
+    ) {
+      throw new Error(usage);
     }
-    return $(input[0]);
+    getInputFromSelector(args.inputSelector);
   }
+}
 
-  /**
-   * Returns the `source` method to be passed to autocomplete.js. It will query
-   * the Algolia index and call the callbacks with the formatted hits.
-   * @function getAutocompleteSource
-   * @returns {function} Method to be passed as the `source` option of
-   * autocomplete
-   */
-  private getAutocompleteSource () {
-    return async (query: string, showSearchResult: (suggestion: Suggestion[]) => void) => {
-      const suggestions = <Suggestion[]><unknown[]>await this.sqlite.search(query, 5);
-      showSearchResult(suggestions.map(suggestion => ({
-        ...suggestion,
-        tags: (<string>suggestion.tags ?? '').split(',').map(str => ({ value: str.trim() })),
-        categories: (<string>suggestion.categories ?? '').split(',').map(str => ({ value: str.trim() })),
-      })));
-      return;
-    };
+function getWorkerFactory (factory?: () => Worker) {
+  if (typeof factory !== 'undefined') {
+    return factory;
   }
-
-  private handleSelected (
-    input: any,
-    _event: any,
-    suggestion: Suggestion,
-    _datasetNumber: any,
-    context: any = {},
-  ) {
-    // Do nothing if click on the suggestion, as it's already a <a href>, the
-    // browser will take care of it. This allow Ctrl-Clicking on results and not
-    // having the main window being redirected as well
-    if (context.selectionMethod === 'click') {
-      return;
-    }
-
-    input.setVal('');
-    window.location.assign(suggestion.url);
-  }
-
-  private handleShown (input: JQuery<HTMLElement>) {
-    // @ts-ignore
-    const middleOfInput = input.offset().left + input.width() / 2;
-    // @ts-ignore
-    let middleOfWindow = $(document).width() / 2;
-
-    if (isNaN(middleOfWindow)) {
-      middleOfWindow = 900;
-    }
-
-    const alignClass =
-      middleOfInput - middleOfWindow >= 0
-        ? 'blogsearch-autocomplete-right'
-        : 'blogsearch-autocomplete-left';
-    const otherAlignClass =
-      middleOfInput - middleOfWindow < 0
-        ? 'blogsearch-autocomplete-right'
-        : 'blogsearch-autocomplete-left';
-    const autocompleteWrapper = $('.blogsearch-autocomplete');
-    if (!autocompleteWrapper.hasClass(alignClass)) {
-      autocompleteWrapper.addClass(alignClass);
-    }
-
-    if (autocompleteWrapper.hasClass(otherAlignClass)) {
-      autocompleteWrapper.removeClass(otherAlignClass);
-    }
-  }
+  // Get current directory for worker
+  const workerDir = typeof window?.blogsearch?.worker === 'function'
+    ? URL.createObjectURL(new Blob([`(${window.blogsearch.worker})()`]))
+    : getCurrentDir('worker.umd.js');
+  return () => new Worker(workerDir);
 }
 
 const getCurrentDir = (() => {
@@ -228,6 +220,20 @@ const getCurrentDir = (() => {
     // This assumes that worker.umd.js is available in the CDN (e.g. JSDelivr).
     return `${curDir.substr(0, curDir.lastIndexOf('/'))}/${fileName}`;
   }
-})(); 
+})();
+
+/**
+ * Returns the matching input from a CSS selector, null if none matches
+ * @function getInputFromSelector
+ * @param  {string} selector CSS selector that matches the search
+ * input of the page
+ */
+function getInputFromSelector (selector: string) {
+  const input = $(selector).filter('input');
+  if (!input?.length) {
+    throw new Error(`Error: No input element in the page matches ${selector}`);
+  }
+  return $(input[0]);
+}
 
 export default BlogSearch;
--- blogsearch/src/lib/index.ts
+++ blogsearch/src/lib/index.ts
@@ -1,6 +1,5 @@
 import BlogSearch from './BlogSearch';
 
-export default async function (args: ConstructorParameters<typeof BlogSearch>[0]): Promise<BlogSearch> {
-  const blogsearch = new BlogSearch(args);
-  return blogsearch.load();
+export default async function (args: Parameters<typeof BlogSearch.create>[0]): Promise<BlogSearch> {
+  return BlogSearch.create(args);
 }
