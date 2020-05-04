536c1e75eff2bfbb8fa7b08932dfebaaebbb8be7
Bumsik Kim <k.bumsik@gmail.com>
2020-05-04 18:44:09 +0900

    gatsby: add support for configuration

--- gatsby-plugin-blogsearch/src/gatsby-node.js
+++ gatsby-plugin-blogsearch/src/gatsby-node.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const fs = require('fs');
+const path = require('path');
 const { Database } = require('blogsearch-crawler/lib/database');
 const { defaultFields } = require('blogsearch-crawler/lib/defaultFields');
 
@@ -17,16 +18,24 @@ const { defaultFields } = require('blogsearch-crawler/lib/defaultFields');
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = async ({ graphql, reporter }, options) => {
-  const filename = 'reactjs.org.gatsby-example.db.wasm';
+  console.log(options);
+
+  // Override existing database
+  const filename = options.output;
+  if (!fs.existsSync(path.dirname(filename))) {
+    fs.mkdirSync(path.dirname(filename))
+  }
   if (fs.existsSync(filename)) {
     fs.unlinkSync(filename);
   }
 
+  // Create database
   const db = await Database.create({
     filename,
     columns: defaultFields,
   });
 
+  // Get siteUrl
   const { siteUrl } = (await graphql(`
     {
       site {
@@ -38,50 +47,19 @@ exports.createPages = async ({ graphql, reporter }, options) => {
   `)).data.site.siteMetadata;
 
   // Query for markdown nodes to use in creating pages.
-  const result = await graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
-            rawMarkdownBody
-            # excerpt
-            # html
-          }
-        }
-      }
-    }
-  `)
+  const result = await graphql(options.query);
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
 
-  result.data.allMarkdownRemark.edges
-    .map(({ node }) => {
-      return {
-        title: node.frontmatter.title,
-        body: node.rawMarkdownBody
-                .replace(/[#`\[\]]+/g, '')
-                .replace(/\(.*\)/g, '')
-                .replace(/\s+/g, ' '),
-        url: siteUrl + node.fields.slug,
-        categories: (() => {
-          const slug = node.fields.slug;
-          return slug ? slug.substring(0, slug.indexOf('/')) : '';
-        })(),
-        tags: '',
-      };
-    })
+  // Index contents in the database
+  options.normalizer(result, siteUrl)
     .map((node, rowid) => {
       db.insert(rowid, node);
     });
 
+  // End
   db.close();
 };
