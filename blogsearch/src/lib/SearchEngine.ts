6fd4f34ff778c120799bb2a0c50f0ac62701fc67
Bumsik Kim <k.bumsik@gmail.com>
2020-05-16 22:44:17 +0900

    Add weights option

--- blogsearch/src/lib/SearchEngine.ts
+++ blogsearch/src/lib/SearchEngine.ts
@@ -1,33 +1,47 @@
 import SQLite from 'sqlite-wasm/lib/WorkerWrapper';
 import { Suggestion } from './autocomplete.js';
 
+const DbName = 'blogsearch';
+const MaxDisplayedTokens = 10;
+enum Column {
+  Title = 'title',
+
+  Body = 'body',
+  Url = 'url',
+  Categories = 'categories',
+  Tags = 'tags',
+};
+
 export interface Config {
   dbPath: string;
   wasmPath: string;
   worker: Worker;
+  weights?: Record<Column, number>
 }
 
-enum Db {
-  TitleIdx = 0,
-
-  BobyIdx,
-  UrlIdx,
-  CategoriesIdx,
-  TagsIdx,
-  DbName = 'blogsearch',
-  MaxDisplayedTokens = 10,
-};
-
 export default class SearchEngine {
-
   private constructor (
     private readonly sqlite: SQLite,
-  ) {}
+    private readonly weightMap: Map<Column, number>,
+    private readonly indexes: Partial<Record<Column, number>> = {},
+  ) {
+    let index = 0;
+    for (const [column] of this.weightMap) {
+      this.indexes[column] = index++;
+    }
+  }
 
   public static async create ({
     dbPath,
     wasmPath,
-    worker
+    worker,
+    weights = {
+      title: 5,
+      body: 1,
+      url: 1, // URL is usually UNINDEXED, therefore this value usually ignored.
+      categories: 3,
+      tags: 3,
+    }
   }: Config)
   : Promise<SearchEngine> {
     /**
@@ -41,14 +55,30 @@ export default class SearchEngine {
     const wasmBuffer = fetch(wasmPath).then(r => r.arrayBuffer());
     const dbBuffer = fetch(dbPath).then(r => r.arrayBuffer());
 
-    const sqlite = await SQLite.init({
-      wasmBinary: await wasmBuffer,
-      worker,
-    });
-    await sqlite.open({
-      dbBinary: await dbBuffer
-    });
-    return new SearchEngine(sqlite);
+    const sqlite = await SQLite.init({ wasmBinary: await wasmBuffer, worker });
+    await sqlite.open({ dbBinary: await dbBuffer });
+
+    // Retrive column info of 'blogsearch' table to get fields and its weights
+    const meta = (await sqlite.run({
+      query: "SELECT `name`, `sql` FROM `sqlite_master` WHERE type='table';"
+    }))[0].values
+      // Columns: ['name'(table name), 'sql'(SQL that created the table)]
+      .find(row => row[0] === DbName);
+    if (typeof meta === 'undefined') throw new Error("Failed to retrive columns information of 'blogsearch'.");
+    // Extract columns from a sql SELECT statement.
+    // Example statement: CREATE VIRTUAL TABLE blogsearch USING fts5( title,body,url UNINDEXED, tokenize = 'porter unicode61 remove_diacritics 1', content=blogsearch_ext_content, content_rowid=rowid)
+    const columns: Column[]= meta[1]?.toString()
+      .replace(/UNINDEXED/g, '') // Remove UNINDEXED column option. The column's weights are ignored anyway.
+      .replace(/[\n|\s]+/g, ' ') // Remove newline
+      .match(/fts5\((.+)\)/)![1]
+      .split(',')
+      .filter(column => !column.includes('=')) // column containing '=' are fts5 option specifiers
+      .map(column => column.trim() as Column);
+    const weightMap = new Map();
+    for (const column of columns) {
+      weightMap.set(column, weights[column] ?? 1);
+    }
+    return new SearchEngine(sqlite, weightMap);
   }
 
   public async search (
@@ -57,33 +87,34 @@ export default class SearchEngine {
     highlightPreTag: string = '<span class="blogsearch-suggestion--highlight">',
     highlightPostTag: string = '</span>',
   ): Promise<Suggestion[]> {
+    const bodyExists = this.weightMap.get(Column.Body) ? true : false;
     // Source: https://www.sqlite.org/fts5.html#the_snippet_function
     const query = `
       SELECT
-        *,
-        snippet(${Db.DbName}, ${Db.BobyIdx}, '{{%%%', '%%%}}', '', ${Db.MaxDisplayedTokens}) as body_highlight
-      FROM ${Db.DbName}
-      WHERE ${Db.DbName} 
+        *
+        ${bodyExists ? `, snippet(${DbName}, ${this.indexes[Column.Body]}, '{{%%%', '%%%}}', '', ${MaxDisplayedTokens}) as body_highlight` : ''}
+      FROM ${DbName}
+      WHERE ${DbName} 
         MATCH '${match}'
-      ORDER BY bm25(${Db.DbName})
+      ORDER BY bm25(${DbName}, ${[...this.weightMap.values()].join(',')})
       LIMIT ${top};
     `;
-    const raw = await this.sqlite.run({ query });
-    if (raw.length === 0) {
-      return [];
-    }
-    // Only one row because only one SQL query is executed.
-    const { columns, values } = raw[0];
+
+    const raw = (await this.sqlite.run({ query }))[0];
+    if (typeof raw === 'undefined') return [];
+    const { columns, values } = raw;
 
     return values
-      .filter(row => row[Db.TitleIdx]) // Filter empty titles
+      .filter(row => row[this.indexes[Column.Title]!] ?? true) // Filter empty titles
       .map(row => {
-        const hightlightIdx = row.length - 1;
-        // hightlight body string
-        // eslint-disable-next-line no-param-reassign
-        row[hightlightIdx] = escapeXMLCharacters(row[hightlightIdx] as string)
-          .replace(/{{%%%/g, highlightPreTag)
-          .replace(/%%%}}/g, highlightPostTag);
+        if (bodyExists) {
+          const hightlightIdx = row.length - 1;
+          // hightlight body string
+          // eslint-disable-next-line no-param-reassign
+          row[hightlightIdx] = escapeXMLCharacters(row[hightlightIdx] as string)
+            .replace(/{{%%%/g, highlightPreTag)
+            .replace(/%%%}}/g, highlightPostTag);
+        }
         return Object.fromEntries(zip(columns, row)) as Suggestion;
       })
       .map(row => ({
@@ -131,7 +162,7 @@ function escapeXMLCharacters (input: string) {
       case '&': return '&amp;';
       case '<': return '&lt;';
       case '>': return '&gt;';
-      default:  throw new Error('Error: XML escape Error.');
+      default:  throw new Error('XML escape Error.');
     }
   });
 }
