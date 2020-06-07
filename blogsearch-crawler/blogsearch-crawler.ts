6a70cf041e55464701aa27d72a38c7fc197b972b
Bumsik Kim <k.bumsik@gmail.com>
2020-06-07 20:35:03 +0900

    crawler: Add support for first optional argument.
    This is for use of a config file other than 'blogsearch.config.js'.
    I need it because I want to add the "slim" version in the examples,
    but I still don't recommend to use it.
    So I left it undocumented.

--- blogsearch-crawler/blogsearch-crawler.ts
+++ blogsearch-crawler/blogsearch-crawler.ts
@@ -1,28 +1,36 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
 import * as path from 'path';
+import * as process from 'process';
 import template from './blogsearch.config.js.template';
 import { UncheckedConfig } from './configTypes';
 import checkConfig from './checkers';
 import crawl from './crawler';
 
-const configPath = path.resolve(process.cwd(), 'blogsearch.config.js');
+const arg = process.argv[2];
+const configFile = arg || 'blogsearch.config.js';
+const configPath = path.resolve(process.cwd(), configFile);
 if (!fs.existsSync(configPath)) {
-  fs.writeFileSync(configPath, template);
-  console.error(`
+  if (arg) {
+    console.error(`${configFile} doesn't exist.`);
+  } else {
+    fs.writeFileSync(configPath, template);
+    console.error(`
 blogsearch.config.js doesn't exist.
 An example blogsearch.config.js is created.
 Visit https://github.com/kbumsik/blogsearch/tree/master/blogsearch-crawler
 to learn how to configure blogsearch.config.js.
 `);
+  }
   process.exit(1);
 }
 
-crawlBlog(require(configPath))
+crawlBlog(tryImport(configPath))
   .then(_value => {
     console.log('Parsing complete.');
     process.exit(0);
-  }).catch(error => {
+  })
+  .catch(error => {
     console.error(error);
     console.log('Parsing failed.');
     process.exit(1);
@@ -43,7 +51,17 @@ async function crawlBlog (uncheckedConfig: UncheckedConfig) {
 
     await crawl(config);
   } catch (error) {
-    // [TODO] Add doc
-    throw new Error(`${error}`);
+    throw new Error(`${error}.
+See: https://github.com/kbumsik/blogsearch/tree/master/blogsearch`);
+  }
+}
+
+function tryImport (configPath: string) {
+  try {
+    return require(configPath); 
+  } catch (error) {
+    console.error(error);
+    console.error(`Parsing ${configPath} failed. Is it a correct .js file?`);
+    process.exit(1);
   }
 }
