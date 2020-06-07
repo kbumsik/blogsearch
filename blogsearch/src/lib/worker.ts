b43f857c02613df79b461655be682a1ab772f2fe
Bumsik Kim <k.bumsik@gmail.com>
2020-06-07 20:47:08 +0900

    worker: add note on the worker.

--- blogsearch/src/lib/worker.ts
+++ blogsearch/src/lib/worker.ts
@@ -1,1 +1,4 @@
+// This copy-and-paste the original worker code from sqlite-wasm (I wrote it).
+// See the source to learn how it works:
+//  https://github.com/kbumsik/sqlite-wasm/blob/master/src/worker.ts
 import 'sqlite-wasm/lib/worker';
